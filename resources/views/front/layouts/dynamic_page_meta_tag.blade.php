a3ecaa1dd78fd335b27607390ceda265b8ff032e
MohdFaraz <farazahmad280@gmail.com>
2024-03-20 11:43:18 +0530

    backup from live

--- resources/views/front/layouts/dynamic_page_meta_tag.blade.php
+++ resources/views/front/layouts/dynamic_page_meta_tag.blade.php
@@ -1,82 +1,30 @@
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 <meta name="robots" content="index, follow" />
-<title>
-  <?php echo ucwords($meta_title); ?>
-</title>
+<title><?php echo ucwords($meta_title); ?></title>
 <meta name="description" content="<?php echo $meta_description; ?>">
 <meta name="keywords" content="<?php echo $meta_keyword; ?>">
 <link rel="canonical" href="<?php echo $page_url; ?>/" />
-<meta name="copyright" content="TutelageStudy.com" />
-<meta name="page-topic" content="<?php echo $page_content; ?>" />
 <meta property="og:title" content="<?php echo $meta_title; ?>" />
 <meta property="og:description" content="<?php echo $meta_description; ?>" />
+<meta property="og:image" content="<?php echo asset($og_image_path); ?>" />
 <link rel="shortcut icon" href="{{ asset('/front/') }}/img/icon.png" type="image/x-icon">
-<meta property="og:locale" content="en_US" />
-<meta property="og:type" content="article" />
-<meta property="og:site_name" content="Tutelage Study Education Consultants : India" />
+<link rel="apple-touch-icon" href="{{ asset('/front/') }}/img/icon.png" />
+<meta property="og:site_name" content="Tutelage Study" />
 <meta property="og:url" content="<?php echo $page_url; ?>/" />
 <meta name="twitter:card" content="summary_large_image" />
-<meta name="twitter:site" content="Tutelage Study MBBS Abroad Consultant" />
+<meta name="twitter:site" content="Tutelage Study" />
 <meta name="twitter:creator" content="@tutelagestudy" />
 <meta name="twitter:url" content="<?php echo $page_url; ?>/" />
 <meta name="twitter:title" content="<?php echo $meta_title; ?>" />
 <meta name="twitter:description" content="<?php echo $meta_description; ?>" />
 <meta name="twitter:image" content="<?php echo asset($og_image_path); ?>" />
 <meta property="twitter:image:type" content="image/jpg" />
-<link rel="apple-touch-icon" href="{{ asset('/front/') }}/img/icon.png" />
-<meta property="og:image" content="<?php echo asset($og_image_path); ?>" />
-
 @include('front.cssjs')
 <!-- Google Tag Manager -->
 <script>
-  (function (w, d, s, l, i) {
-    w[l] = w[l] || [];
-    w[l].push({
-      'gtm.start': new Date().getTime(),
-      event: 'gtm.js'
-    });
-    var f = d.getElementsByTagName(s)[0],
-      j = d.createElement(s),
-      dl = l != 'dataLayer' ? '&l=' + l : '';
-    j.async = true;
-    j.src =
-      'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
-    f.parentNode.insertBefore(j, f);
-  })(window, document, 'script', 'dataLayer', 'GTM-T4ZDHCD');
+!function(e,t,a,r,g){e[r]=e[r]||[],e[r].push({"gtm.start":new Date().getTime(),event:"gtm.js"});var n=t.getElementsByTagName(a)[0],s=t.createElement(a);s.async=!0,s.src="https://www.googletagmanager.com/gtm.js?id="+g+("dataLayer"!=r?"&l="+r:""),n.parentNode.insertBefore(s,n)}(window,document,"script","dataLayer","GTM-T4ZDHCD");
 </script>
-<!-- End Google Tag Manager -->
-<!-- organization schema code -->
 <script type="application/ld+json">
-  {
-    "@context": "https://schema.org",
-    "@type": "Organization",
-    "@id":"https://www.tutelagestudy.com/#organization",
-    "name": "Tutelage Study",
-    "url": "https://www.tutelagestudy.com/",
-    "logo": "{{ asset('/front/') }}/img/logo_light.png",
-    "address": {
-      "@type": "PostalAddress",
-      "streetAddress": "B-16 Ground Floor, Mayfield Garden, Sector 50",
-      "addressLocality": "Gurugram",
-      "addressRegion": "Haryana",
-      "postalCode": "122002",
-      "addressCountry": "India"
-    },
-    "contactPoint": {
-      "@type": "ContactPoint",
-      "contactType": "contact",
-      "telephone": "+91-9870406867",
-      "email": "info@tutelagestudy.com"
-    },
-    "sameAs": [
-      "https://www.facebook.com/tutelagestudyabroad/",
-      "https://twitter.com/tutelagestudy",
-      "https://www.youtube.com/channel/UCK2eeC1CkS3YkYrGnnzBUEQ",
-      "https://in.pinterest.com/tutelagestudy/",
-      "https://in.linkedin.com/in/tutelage-study-4085a51ab",
-      "https://www.instagram.com/tutelagestudy/",
-      "https://www.tumblr.com/tutelagestudyabroad/"
-    ]
-  }
+ {"@context":"https://schema.org","@type":"Organization","@id":"https://www.tutelagestudy.com/#organization","name":"Tutelage Study","url":"https://www.tutelagestudy.com/","logo":"https://www.tutelagestudy.com/front/img/logo_light.png","address":{"@type":"PostalAddress","streetAddress":"B-16 Ground Floor, Mayfield Garden, Sector 50","addressLocality":"Gurugram","addressRegion":"Haryana","postalCode":"122002","addressCountry":"IN"},"contactPoint":{"@type":"ContactPoint","contactType":"contact","telephone":"+91-9870406867","email":"info@tutelagestudy.com"},"sameAs":["https://www.facebook.com/tutelagestudyabroad/","https://twitter.com/tutelagestudy","https://www.youtube.com/channel/UCK2eeC1CkS3YkYrGnnzBUEQ","https://in.pinterest.com/tutelagestudy/","https://in.linkedin.com/in/tutelage-study-4085a51ab","https://www.instagram.com/tutelagestudy/"]}
 </script>
