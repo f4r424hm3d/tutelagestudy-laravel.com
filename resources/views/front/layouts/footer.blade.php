f25c932716cb68f2a929b1aaefc830efcf876473
MohdFaraz <farazahmad280@gmail.com>
2024-09-13 13:20:02 +0530

    Add Tawk.to

--- resources/views/front/layouts/footer.blade.php
+++ resources/views/front/layouts/footer.blade.php
@@ -414,7 +414,7 @@ $finalUrl = url($form_url);
     });
   });
 </script>
-<!--Start of Tawk.to Script
+<!--Start of Tawk.to Script-->
 <script type="text/javascript">
   var Tawk_API = Tawk_API || {},
     Tawk_LoadStart = new Date();
@@ -422,7 +422,7 @@ $finalUrl = url($form_url);
     var s1 = document.createElement("script"),
       s0 = document.getElementsByTagName("script")[0];
     s1.async = true;
-    s1.src = 'https://embed.tawk.to/5ef05b1b9e5f694422910b32/default';
+    s1.src = 'https://embed.tawk.to/66e3ed9fea492f34bc12fa1d/1i7l5ge59';
     s1.charset = 'UTF-8';
     s1.setAttribute('crossorigin', '*');
     s0.parentNode.insertBefore(s1, s0);
