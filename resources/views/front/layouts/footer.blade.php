0eb0392cf4b009d44093077190cb219ec0766475
MohdFaraz <farazahmad280@gmail.com>
2025-06-10 13:46:50 +0530

    remove tawk.to

--- resources/views/front/layouts/footer.blade.php
+++ resources/views/front/layouts/footer.blade.php
@@ -398,21 +398,7 @@
     });
   });
 </script>
-<!--Start of Tawk.to Script-->
-<script type="text/javascript">
-  var Tawk_API = Tawk_API || {},
-    Tawk_LoadStart = new Date();
-  (function() {
-    var s1 = document.createElement("script"),
-      s0 = document.getElementsByTagName("script")[0];
-    s1.async = true;
-    s1.src = 'https://embed.tawk.to/66e3ed9fea492f34bc12fa1d/1i7l5ge59';
-    s1.charset = 'UTF-8';
-    s1.setAttribute('crossorigin', '*');
-    s0.parentNode.insertBefore(s1, s0);
-  })();
-</script>
-<!--End of Tawk.to Script-->
+
 </body>
 
 </html>
