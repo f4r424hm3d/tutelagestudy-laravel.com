83ead45cf0acb1df73dc8eb5b1c9c9c5babfe44a
MohdFaraz <farazahmad280@gmail.com>
2025-06-30 12:10:32 +0530

    Deleted extra files

--- resources/views/front/header-filter.blade.php
+++
@@ -1,5 +0,0 @@
-<header class="header header--mobile header--mobile-categories" data-sticky="true">
-  <div class="header__filter">
-    <button class="ps-shop__filter-mb" id="filter-sidebar"><i class="icon-equalizer"></i><span>Filter</span></button>
-  </div>
-</header>
--- resources/views/front/mbl-filter.blade.php
+++
@@ -1,9 +0,0 @@
-<div class="ps-filter--sidebar">
-  <div class="ps-filter__header">
-    <h3>Filter Products</h3>
-    <a class="ps-btn--close ps-btn--no-boder" href="#"></a>
-  </div>
-  <div class="ps-filter__content">
-    <?php $this->load->view('web/filter'); ?>
-  </div>
-</div>
